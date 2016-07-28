895359113b3c50498ad9f5c5a7ad1638a017d901
Pablo Ezequiel <@no_cuestanada>
2016-07-28 02:33:37 -0300

    First UI Version - a little logic behind some buttons

--- ChessLab_v01/ChessLab_v01/ViewController.m
+++ ChessLab_v01/ChessLab_v01/ViewController.m
@@ -10,9 +10,52 @@
 
 @interface ViewController ()
 
+
+@property (weak, nonatomic) IBOutlet UIImageView *img_board;
+@property (weak, nonatomic) IBOutlet UILabel *txt_hint_solution;
+
+
+
 @end
 
 @implementation ViewController
+
+int counter = 1;
+
+- (IBAction)btn_next:(id)sender {
+
+    NSLog(@"bnt_hint next");
+    counter++;
+    _img_board.image = [UIImage imageNamed:[NSString stringWithFormat:@"chess_mate1_0000%d.png", counter]];
+    
+
+
+}
+
+- (IBAction)btn_hint:(id)sender {
+    
+    NSLog(@"bnt_hint click");
+
+    // 1. ♖xc5+ ♚b6 2. ♕c7+ ♚a7 3. ♖xa5+ Qa6 4. ♗d4#
+    
+    if (counter == 1) {
+         _txt_hint_solution.text = @"1. ♕d6+ ♚e8 2. ♗d7+ ♚d8 3. ♗xb5+ ♚c8 4. ♗a6#";
+    }
+    if (counter == 2) {
+        _txt_hint_solution.text = @"1. ♖xc5+ ♚b6 2. ♕c7+ ♚a7 3. ♖xa5+ ♛a6 4. ♗d4#";
+    }
+    if (counter == 3) {
+        _txt_hint_solution.text = @"1. ♗g5+ ♜f6 2. ♗xf6+ gxf6 3. ♕g7+ ♚e8 4. ♕f7#";
+    }
+    if (counter == 4) {
+        _txt_hint_solution.text = @"1. ♕xh7+ ♚xh7 2. ♖h3+ ♝xh3 3. ♖xh3+ ♚g6 4. ♖h6#";
+    }
+    if (counter == 5) {
+        _txt_hint_solution.text = @"1. ♘c7+ ♛xc7 2. ♕xf7+ ♝xf7 3. ♗xf7#";
+    }
+ 
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
